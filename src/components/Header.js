576524809d77b92c8dfa9a803f950f4c2dbc9df5
Vanessa <vanessa.mercado24@gmail.com>
2022-05-27 13:47:26 -0700

    add react icons

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,13 +1,13 @@
 import React from 'react'
+import HeaderImage from "../images/header.jpeg";
 
 const Header = () => {
     return (
-        <header className="text-gray-100 pt-16 pb-10 text-center">
-            <h1 className="font-semibold font-rammetto lg:text-5xl sm:text-4xl text-2xl pb-4">
-                Welcome to the Health Room
-            </h1>
-            <p className=" lg:text-2xl sm:text-lg text-sm font-light">Your one stop blog for health and fitness information</p>
-        </header>
+        <section class="w-full h-screen">
+            <img src={HeaderImage} class="object-cover w-full h-full" alt="tree-person-walking" />
+
+        </section>
+
     )
 }
 
--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,8 +1,29 @@
 import React from 'react'
 import {Link} from 'gatsby'
+import { HiOutlineMenuAlt3 } from "@react-icons/all-files/hi/HiOutlineMenuAlt3";
+
 
 const Layout = ({children}) => {
-    const navbarStyle = `
+    const navbarStyles = `
+    md:static
+    fixed
+    bottom-0
+    top-12
+    md:flex
+    md:space-x-7
+    items-center
+    md:bg-transparent
+    bg-red-500
+    bg-opacity-50
+    md:w-auto
+    w-10/12
+    md:text-gray-500
+    text-white
+    md:space-y-0
+    space-y-5
+    p-2
+    `
+    const fullNavbarStyle = `
     flex
     justify-end
     shadow-md
@@ -11,7 +32,6 @@ const Layout = ({children}) => {
     pb-4
     `
     const linkStyle = `
-    mr-8
     text-zinc-500
     hover:text-zinc-800
     `
@@ -27,8 +47,14 @@ const Layout = ({children}) => {
 
     return (
         <div>
-            <nav className="rounded-lg">
-                <ul className={navbarStyle}>
+            <nav className="md:flex justify-between items-center bg-white sticky top-0 z-20">
+                <div className="flex items-center justify-between">
+                <a href="/">
+                    Logo<HiOutlineMenuAlt3 />
+                </a>
+                <HiOutlineMenuAlt3 />
+                </div>
+                <ul className={navbarStyles}>
                     <li className={linkStyle}><Link to="/about">about</Link></li>
                     <li className={linkStyle}><Link to="/projects">projects</Link></li>
                     <li className={linkStyle}><Link to="/blog">blog</Link></li>
