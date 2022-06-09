cb0253f6fe362328a5aaf91dbb3e1209ff183ba9
Vanessa <vanessa.mercado24@gmail.com>
2022-06-08 21:00:46 -0700

    wip

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,7 +9,23 @@ module.exports = {
     extend: {
       transitionProperty: {
         'left': 'left',
+      },
+      keyframes: {
+        wiggle: {
+          '0%, 100%': { transform: 'rotate(-3deg)' },
+          '50%': { transform: 'rotate(3deg)' },
+        },
+        showDiv: {
+          '99%': { visibility: 'hidden' },
+          '100%': { visibility: 'visible' },
+        }
+      },
+      animation: {
+        wiggle: 'wiggle 1s ease-in-out infinite',
+        showDiv: '3s fadeIn',
       }
+
+
     }
   },
   plugins: [],
